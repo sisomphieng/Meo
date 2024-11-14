6a3e65a86231717d38755d96efe2c369f173748c
sisomphieng <meo@iuj.ac.jp>
2024-11-14 23:16:34 +0900

    Update Test.py

--- Test.py
+++ Test.py
@@ -395,4 +395,4 @@ if st.session_state.page == "final_page":
             except Exception as e:
                 st.error(f"Error building portfolio: {e}")
     #else:
-        st.warning("Please complete the Risk Tolerance Quiz first to get stock suggestions.")
+        #st.warning("Please complete the Risk Tolerance Quiz first to get stock suggestions.")
